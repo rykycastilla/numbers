fbfa4c6c272c5af0a951b941eb379cdcf39ec72d
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2025-08-22 11:31:07 -0400

    fix: Using real pixels instead of viewport values in useBoardPosition

--- src/hooks/board_position.ts
+++ src/hooks/board_position.ts
@@ -1,10 +1,9 @@
 import resolveAsMatrix from '../functions/resolve_as_matrix'
 import { MARGIN } from '../data/styles.json'
-import { useVP } from 'react-native-viewport-provider'
 
 // Set the position value using the previous objects
-function calcPosition( gridPos:number, itemSize:string ): string {
-  return  `( ${ itemSize } + ${ MARGIN } ) * ${ gridPos }`
+function calcPosition( gridPos:number, itemSize:number ): number {
+  return  ( itemSize + MARGIN ) * gridPos
 }
 
 interface Position {
@@ -12,21 +11,12 @@ interface Position {
   left: number,
 }
 
-// These are single Position properties with viewport units (them need to be parsed)
-interface RawPosition {
-  top: string,
-  left: string,
-}
-
-function useBoardPosition( position:number, itemSize:string, gridSize:number ): Position {
+function useBoardPosition( position:number, itemSize:number, gridSize:number ): Position {
   const { x, y } = resolveAsMatrix( position, gridSize )  // Calculating grid position
   // Calculating coordinates based on the grid
-  const top: string = calcPosition( y, itemSize ),
-    left: string = calcPosition( x, itemSize ),
-    rawPosition: RawPosition = { top, left }
-  // Parsing coordinates as viewport values
-  const calculatedPosition = useVP( rawPosition ) as Position
-  return calculatedPosition
+  const top: number = calcPosition( y, itemSize ),
+    left: number = calcPosition( x, itemSize )
+  return { top, left }
 }
 
 export default useBoardPosition
